1811ad626c33affe224c6664ab1213f0194da12e
AlexButeau <alexbuteau1@gmail.com>
2020-11-19 19:23:31 +0100

    first version of quest cards and quest list

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './Navbar';
 // import Agenda from './Agenda';
 import QuestsList from './QuestsList';
+import './styles/Home.scss';
 
 const Home = () => {
   return (
     <div className='home'>
       {/* <Agenda /> */}
       <Navbar />
-      {/* <QuestsList /> */}
+      <QuestsList />
     </div>
   );
 };
