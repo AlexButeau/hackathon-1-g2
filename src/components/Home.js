fd220e844456517d975000bec8fee54bf026c94f
AlexButeau <alexbuteau1@gmail.com>
2020-11-19 23:33:54 +0100

    changed a component called twice

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
-import './styles/Agenda.scss'
+import './styles/Agenda.scss';
 import FullCalendar from 'fullcalendar-reactwrapper';
 import QuestsList from './QuestsList';
 import axios from 'axios';
 import Navbar from './Navbar';
 import QuestPost from './QuestPost';
-import QuestsList from './QuestsList';
 import './styles/Home.scss';
 
 const Home = () => {
   const [events, setEvents] = useState([]);
-        useEffect(() =>{
-          axios.get("https://kaamelot-server.herokuapp.com/agenda")
-              .then((res) => res.data)
-              .then((data)=>{
-                  console.log(data);
-                  setEvents(data);
-              });
-      },[]);
+  useEffect(() => {
+    axios
+      .get('https://kaamelot-server.herokuapp.com/agenda')
+      .then((res) => res.data)
+      .then((data) => {
+        console.log(data);
+        setEvents(data);
+      });
+  }, []);
   return (
     <div className='home'>
-
       <div className='agendaSection'>
-      <FullCalendar
-            id="agenda"
-            header={{
-              center: '',
-              right: 'prev,next'
-            }}
-            defaultDate = {moment('470-11-20').format('YYYY-MM-DD')}
-            events={events}
-            navLinks={true} // can click day/week names to navigate views
-            editable={true}
-            height={450}
-            today={false}
-            selectable={true}
-            eventLimit={true} // allow "more" link when too many events
+        <FullCalendar
+          id='agenda'
+          header={{
+            center: '',
+            right: 'prev,next',
+          }}
+          defaultDate={moment('470-11-20').format('YYYY-MM-DD')}
+          events={events}
+          navLinks={true} // can click day/week names to navigate views
+          editable={true}
+          height={450}
+          today={false}
+          selectable={true}
+          eventLimit={true} // allow "more" link when too many events
         />
-        </div>
+      </div>
 
       <Navbar />
- <QuestsList />
+      <QuestsList />
       <QuestPost />
-
     </div>
   );
 };
