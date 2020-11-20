041376896e3168da50b881f657cb8c901e0e401b
AlexButeau <alexbuteau1@gmail.com>
2020-11-20 02:48:14 +0100

    changed some css

--- src/components/QuestPage.js
+++ src/components/QuestPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import QuestPost from './QuestPost';
 import QuestsList from './QuestsList';
+import './styles/QuestPage.scss';
 
 const QuestPage = () => {
   return (
-    <div>
+    <div className='quest-list-page'>
       <QuestsList />
       <QuestPost />
     </div>
